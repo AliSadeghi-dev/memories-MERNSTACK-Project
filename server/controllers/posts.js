a55d4f43b3cafefdd26df5e4ced58aa93addb251
alisadeghi <alisadghi228@gmail.com>
2022-12-22 17:54:03 +0330

    fix some bugs

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,7 +5,6 @@ const PostMessage = require("../models/postMessage");
 exports.getPosts = async(req, res) => {
     try {
         const postMessage = await PostMessage.find();
-        console.log(postMessage);
         res.status(200).json(postMessage);
     } catch (error) {
         res.status(404).json({ message: error.message });
